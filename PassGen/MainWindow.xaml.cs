ef4b4d3fcbfefbc75c9321ecd2d7f0408a3246a9
yorha22b <jkantorowicz05@gmail.com>
2023-02-21 13:42:26 +0100

    The original bare bones version of password generator

---
+++ PassGen/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PassGen
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        PasswordGenerator generator = new();
+        string allowedCharacters = "abcdefghijklmnoprstuwxyzABCDEFGHIJKLMNOPRSTUWXYZ1234567890";
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            output_text_block.Text = generator.generatePassword(Convert.ToInt16(slValue.Value), allowedCharacters);
+        }
+    }
+}
---
+++ PassGen/PasswordGenerator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PassGen
+{
+    class PasswordGenerator
+    {
+        public string generatePassword(int passLength, string allowedCharacters)
+        {
+            StringBuilder pass = new StringBuilder();
+            Random rdn = new Random();
+            for (int i = 0; i < passLength; i++)
+            {
+                pass.Append(allowedCharacters[(int)rdn.NextInt64(0, allowedCharacters.Length)]);
+            }
+            return pass.ToString();
+        }
+    }
+}
+
