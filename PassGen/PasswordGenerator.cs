866986b2eef20a0317e67c974147f73a30b93423
yorha22b <jkantorowicz05@gmail.com>
2023-02-21 19:46:12 +0100

    the functionality update

--- PassGen/PasswordGenerator.cs
+++ PassGen/PasswordGenerator.cs
@@ -8,13 +8,14 @@ namespace PassGen
 {
     class PasswordGenerator
     {
+
         public string generatePassword(int passLength, string allowedCharacters)
         {
             StringBuilder pass = new StringBuilder();
             Random rdn = new Random();
             for (int i = 0; i < passLength; i++)
             {
-                pass.Append(allowedCharacters[(int)rdn.NextInt64(0, allowedCharacters.Length)]);
+                if(allowedCharacters.Length>0) pass.Append(allowedCharacters[(int)rdn.NextInt64(0, allowedCharacters.Length)]);
             }
             return pass.ToString();
         }
